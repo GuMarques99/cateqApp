210705e90d2b07410a30406358baec5b16eda52d
Gustavo Marques Gonçalves <gustavo.m.goncalves99@gmail.com>
2023-05-16 07:26:03 -0300

    Em andamento: modificação no turmaService

--- src/app/turmas/turmas.page.ts
+++ src/app/turmas/turmas.page.ts
@@ -14,56 +14,22 @@ import { TurmasService } from '../services/turmas.service';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class TurmasPage implements OnInit {
-  public listaTurmas: Turma[] = /*[];
-  public turmasFixas: Turma[] =*/ [
-  {
-    id: '1',
-    apelido: 'Turma 1',
-    descricao: 'Descrição da Turma 1',
-    dataInicio: new Date('2023-04-01'),
-    dataConclusao: new Date('2023-05-01'),
-    dataPrevEncerramento: new Date('2023-06-01'),
-    dataCadastro: new Date(),
-    responsavel: 'Responsável 1',
-    status: statusTurma.pendente
-  },
-  {
-    id: '2',
-    apelido: 'Turma 2',
-    descricao: 'Descrição da Turma 2',
-    dataInicio: new Date('2023-04-02'),
-    dataConclusao: new Date('2023-05-02'),
-    dataPrevEncerramento: new Date('2023-06-02'),
-    dataCadastro: new Date(),
-    responsavel: 'Responsável 2',
-    status: statusTurma.emAndamento
-  },
-  {
-    id: '3',
-    apelido: 'Turma 3',
-    descricao: 'Descrição da Turma 3',
-    dataInicio: new Date('2023-04-03'),
-    dataConclusao: new Date('2023-05-03'),
-    dataPrevEncerramento: new Date('2023-06-03'),
-    dataCadastro: new Date(),
-    responsavel: 'Responsável 3',
-    status: statusTurma.finalizada
-  },
-  // Adicione os outros elementos da mesma forma
-];
+  public listaTurmas: Turma[] = [];
 
-  constructor(/*private turmasServ: TurmasService*/) { }
+  constructor(private turmasServ: TurmasService) { }
 
   ngOnInit() {
     this.listarTurmas();
   }
 
   public listarTurmas() {
-    //.turmasServ.carregar().then((turmas) => {
-    //  this.listaTurmas = turmas
-    //}).then(() => {
-      //this.listaTurmas.concat(this.turmasFixas);
-    //});
+    this.turmasServ.carregar().then((turmas) => {
+      this.listaTurmas = turmas
+    }) 
+  }
+
+  public adicionar() {
+    
   }
 
   public statusBadgeColor(status: statusTurma): string {
